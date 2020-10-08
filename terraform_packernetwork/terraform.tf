90cb12638db7f7ba999009213979ab46dc3faa82
James Fogarty <jayfo@users.noreply.github.com>
2020-10-08 13:15:28 -0700

    Enable Hostnames

--- terraform_packernetwork/terraform.tf
+++ terraform_packernetwork/terraform.tf
@@ -57,6 +57,7 @@ provider "aws" {
 resource "aws_vpc" "vpc" {
   cidr_block         = "10.0.0.0/16"
   enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = merge(
     local.project_tags,
