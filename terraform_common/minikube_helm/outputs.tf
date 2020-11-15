536a802896a5c75c8d60fcb88c30f16492370c44
James Fogarty <jayfo@users.noreply.github.com>
2020-11-15 11:12:08 -0800

    Add Instance ID to Output

--- terraform_common/minikube_helm/outputs.tf
+++ terraform_common/minikube_helm/outputs.tf
@@ -1,10 +1,20 @@
 /*
+ * ID of the instance.
+ */
+output "id" {
+  value = aws_instance.minikube.id
+}
+
+/*
  * Public IP of the instance.
  */
 output "ip" {
   value = aws_instance.minikube.public_ip
 }
 
+/*
+ * Path to an identify file for SSH access.
+ */
 output "identity_file" {
   value = local_file.instance_key_private.filename
 }
