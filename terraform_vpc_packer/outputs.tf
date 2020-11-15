d19eecacbdd6ef3e033fa2fa40aa2340304f8bee
James Fogarty <jayfo@users.noreply.github.com>
2020-11-15 08:40:12 -0800

    Fix Comments

--- terraform_vpc_packer/outputs.tf
+++ terraform_vpc_packer/outputs.tf
@@ -1,10 +1,13 @@
 /*
- * Public IP of the instance.
+ * VPC created for use by Packer.
  */
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
+/*
+ * Subnet created for use by Packer.
+ */
 output "subnet_id" {
   value = module.vpc.subnet_id
 }
