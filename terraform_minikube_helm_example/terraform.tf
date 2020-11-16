04997594e2e75263a5cd753d3791efa5a1739a4f
James Fogarty <jayfo@users.noreply.github.com>
2020-11-16 07:49:10 -0800

    Add Minikube Helm Support for Providing an Elastic IP

--- terraform_minikube_helm_example/terraform.tf
+++ terraform_minikube_helm_example/terraform.tf
@@ -20,13 +20,20 @@ module "minikube_helm_instance_1" {
   ami_architecture = "amd64"
   aws_instance_type = "t3.medium"
 
+  eip = false
+
   tags = {
   }
 }
 
 /*
  * Instance of Minikube Helm.
+ *
+ * Associated with a provided elastic IP.
  */
+resource "aws_eip" "ip" {
+}
+
 module "minikube_helm_instance_2" {
   source = "../terraform_common/minikube_helm"
 
@@ -37,6 +44,10 @@ module "minikube_helm_instance_2" {
   aws_availability_zone = "us-east-1a"
   ami_architecture = "amd64"
   aws_instance_type = "t3.medium"
+
+  eip = true
+  eip_id = aws_eip.ip.id
+  eip_public_ip = aws_eip.ip.public_ip
 
   tags = {
   }
