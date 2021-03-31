39c2bd116012096adc2ab39655b658f000b2d155
James Fogarty <jayfo@users.noreply.github.com>
2021-03-31 07:01:31 -0700

    Include requirements

--- aws_infrastructure/setup.py
+++ aws_infrastructure/setup.py
@@ -1,20 +1,26 @@
 import datetime
 import setuptools
 
+# Semantic Versioning does not allow leading zeroes in dot-separated identifiers,
+# this datetime format will increment but is intentionally not dot-separated
 build_time = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
 
 setuptools.setup(
     name='aws_infrastructure',
-    version='0.1.0.{}'.format(build_time),
+    # Should follow Semantic Versioning <https://semver.org/>
+    version='0.1.0-dev{}'.format(build_time),
     # author='',
     # author_email='',
     packages=setuptools.find_packages(),
-    scripts=[
-    ],
     url='https://github.com/fogies/aws-infrastructure',
     # license='',
     # description='',
     # long_description='',
+    python_requires='>=3',
     install_requires=[
+        'invoke>=1.5',
+        'paramiko>=2.7.2',
+        'ruamel.yaml>=.17,<.18',
+        'semver>=3.0.0.dev2'
     ],
 )
