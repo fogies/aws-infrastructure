8c89c3c00c8ca4084f9d7af22eef8eab7312f1b2
James Fogarty <jayfo@users.noreply.github.com>
2021-08-18 07:16:24 -0700

    Clean Up Comment

--- examples/minikube_helm/terraform.tf
+++ examples/minikube_helm/terraform.tf
@@ -7,13 +7,14 @@ terraform {
 }
 
 /*
- * Instance of Minikube Helm.
- *
- * Associated with a provided elastic IP.
+ * IP to provide to instance of Minikube Helm.
  */
 resource "aws_eip" "ip" {
 }
 
+/*
+ * Instance of Minikube Helm.
+ */
 module "minikube_helm_instance" {
   source = "../../terraform_common/minikube_helm"
 
