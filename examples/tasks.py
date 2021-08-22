969910e987b2c9b9e80588f9f8ff4a23ba4a370b
James Fogarty <jayfo@users.noreply.github.com>
2021-08-22 12:01:07 -0700

    Example ECR Task

--- examples/tasks.py
+++ examples/tasks.py
@@ -2,6 +2,7 @@ from invoke import Collection
 
 from aws_infrastructure.tasks import compose_collection
 import examples.codebuild.tasks
+import examples.ecr.tasks
 import examples.minikube_helm.tasks
 import examples.minikube_helm_multiple.tasks
 
@@ -10,6 +11,9 @@ ns = Collection('examples')
 
 # Compose from codebuild
 compose_collection(ns, examples.codebuild.tasks.ns)
+
+# Compose from ecr-simple
+compose_collection(ns, examples.ecr.tasks.ns)
 
 # Compose from minikube-helm
 compose_collection(ns, examples.minikube_helm.tasks.ns)
