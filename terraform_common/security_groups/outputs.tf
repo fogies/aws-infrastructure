1233c2b7f8293eced3c5830f329dd636dea23602
James Fogarty <jayfo@users.noreply.github.com>
2021-08-28 05:54:25 -0700

    Fix Copy-Paste Bug

--- terraform_common/security_groups/outputs.tf
+++ terraform_common/security_groups/outputs.tf
@@ -2,9 +2,9 @@
  * List of IDs of created security groups.
  */
 output "security_group_ids" {
-  value = compact(list(
-    var.allow_egress_to_anywhere ? aws_security_group.allow_egress_to_anywhere[0].id : 0,
-    var.allow_egress_to_anywhere ? aws_security_group.allow_http_from_anywhere[0].id : 0,
-    var.allow_egress_to_anywhere ? aws_security_group.allow_ssh_from_anywhere[0].id : 0,
-  ))
+  value = compact([
+    var.allow_egress_to_anywhere ? one(aws_security_group.allow_egress_to_anywhere).id : null,
+    var.allow_http_from_anywhere ? one(aws_security_group.allow_http_from_anywhere).id : null,
+    var.allow_ssh_from_anywhere ? one(aws_security_group.allow_ssh_from_anywhere).id : null,
+  ])
 }
