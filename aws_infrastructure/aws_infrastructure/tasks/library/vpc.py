b0d2d423d4b65ea162c8805abaf56cff41a87ce7
James Fogarty <jayfo@users.noreply.github.com>
2021-10-04 14:27:23 -0700

    Initial Attempt at VPC Tasks

---
+++ aws_infrastructure/aws_infrastructure/tasks/library/vpc.py
@@ -0,0 +1,62 @@
+from aws_infrastructure.tasks import compose_collection
+import aws_infrastructure.tasks.library.terraform
+from collections import namedtuple
+from invoke import Collection
+from pathlib import Path
+from typing import List
+from typing import Union
+
+
+def create_tasks(
+    *,
+    config_key: str,
+    bin_terraform: Union[Path, str],
+    dir_terraform: Union[Path, str],
+):
+    """
+    Create all of the tasks, re-using and passing parameters appropriately.
+    """
+
+    bin_terraform = Path(bin_terraform)
+    dir_terraform = Path(dir_terraform)
+
+    ns_vpc = Collection('vpc')
+
+    ns_terraform = aws_infrastructure.tasks.library.terraform.create_tasks(
+        config_key=config_key,
+        bin_terraform=bin_terraform,
+        dir_terraform=dir_terraform,
+        output_tuple_factory=namedtuple(
+            'vpc',
+            [
+                'vpc_id',
+                'default_security_group_id',
+                'subnet_id',
+                'subnet_ids',
+            ]
+        ),
+        output_enhance=_output_enhance,
+    )
+
+    compose_collection(
+        ns_vpc,
+        ns_terraform,
+        sub=False
+    )
+
+    return ns_vpc
+
+
+def create_vpc_read_only(
+    ns_vpc: Collection,
+):
+    """
+    Create a read only context manager.
+    """
+
+    vpc_read_only = aws_infrastructure.tasks.library.terraform.create_context_manager_read_only(
+        init=ns_vpc.tasks['init'],
+        output=ns_vpc.tasks['output'],
+    )
+
+    return vpc_read_only
