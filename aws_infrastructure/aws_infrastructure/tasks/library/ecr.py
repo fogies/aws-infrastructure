99a206fe78a439b5daa35ddd8ea86d2b6e455c94
James Fogarty <jayfo@users.noreply.github.com>
2021-11-16 07:30:35 -0800

    Configure Boto

--- aws_infrastructure/aws_infrastructure/tasks/library/ecr.py
+++ aws_infrastructure/aws_infrastructure/tasks/library/ecr.py
@@ -2,6 +2,7 @@ from aws_infrastructure.tasks import compose_collection
 import aws_infrastructure.tasks.library.terraform
 import base64
 import boto3
+import botocore
 from collections import namedtuple
 from invoke import Collection
 from pathlib import Path
@@ -12,39 +13,57 @@ from typing import Union
 
 def _output_enhance(
     *,
-    context,
-    output,
+    aws_profile: str,
+    aws_shared_credentials_path: Path,
+    aws_config_path: Path,
 ):
-    """
-    Enhance the Terraform output with registry authentication information.
-    """
+    def output_enhance(
+        *,
+        context,
+        output,
+    ):
+        """
+        Enhance the Terraform output with registry authentication information.
+        """
 
-    # Obtain an authorization token
-    boto_ecr = boto3.client('ecr')
-    token = boto_ecr.get_authorization_token()['authorizationData'][0]['authorizationToken']
+        # Obtain an authorization token
+        # boto already checked the environment variables, so setting them now has no effect on the default session.
+        # Creating a new session prompts boto to check again.
+        # We could set/unset the environment variables, but instead configure directly within the boto session.
+        boto_session = boto3.Session(botocore_session=botocore.session.Session(
+            session_vars={
+                'profile': (None, None, aws_profile, None),
+                'credentials_file': (None, None, aws_shared_credentials_path, None),
+                'config_file': (None, None, aws_config_path, None),
+            }
+        ))
+        boto_ecr = boto3.client('ecr')
+        token = boto_ecr.get_authorization_token()['authorizationData'][0]['authorizationToken']
 
-    # Token is base64 encoded, contents are 'user:password'
-    token_decoded = base64.standard_b64decode(token).decode('UTF-8')
-    match = re.match('(.+):(.+)', token_decoded)
-    token_user = match.group(1)
-    token_password = match.group(2)
+        # Token is base64 encoded, contents are 'user:password'
+        token_decoded = base64.standard_b64decode(token).decode('UTF-8')
+        match = re.match('(.+):(.+)', token_decoded)
+        token_user = match.group(1)
+        token_password = match.group(2)
 
-    output = namedtuple(
-        'ecr',
-        [
-            'registry_url',
-            'registry_user',
-            'registry_password',
-            'repository_urls',
-        ]
-    )(
-        registry_url=output.registry_url,
-        registry_user=token_user,
-        registry_password=token_password,
-        repository_urls=output.repository_urls,
-    )
+        output = namedtuple(
+            'ecr',
+            [
+                'registry_url',
+                'registry_user',
+                'registry_password',
+                'repository_urls',
+            ]
+        )(
+            registry_url=output.registry_url,
+            registry_user=token_user,
+            registry_password=token_password,
+            repository_urls=output.repository_urls,
+        )
 
-    return output
+        return output
+
+    return output_enhance
 
 
 def create_tasks(
@@ -52,6 +71,9 @@ def create_tasks(
     config_key: str,
     terraform_bin: Union[Path, str],
     terraform_dir: Union[Path, str],
+    aws_profile: str,
+    aws_shared_credentials_path: Union[Path, str],
+    aws_config_path: Union[Path, str],
 ):
     """
     Create all of the tasks, re-using and passing parameters appropriately.
@@ -59,6 +81,8 @@ def create_tasks(
 
     terraform_bin = Path(terraform_bin)
     terraform_dir = Path(terraform_dir)
+    aws_shared_credentials_path = Path(aws_shared_credentials_path)
+    aws_config_path = Path(aws_config_path)
 
     ns_ecr = Collection('ecr')
 
@@ -73,7 +97,11 @@ def create_tasks(
                 'repository_urls'
             ]
         ),
-        output_enhance=_output_enhance,
+        output_enhance=_output_enhance(
+            aws_profile=aws_profile,
+            aws_shared_credentials_path=aws_shared_credentials_path,
+            aws_config_path=aws_config_path,
+        ),
     )
 
     compose_collection(
