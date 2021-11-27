366e971e02d4455dfbb4c4deb6181c49aa3de34a
James Fogarty <jayfo@users.noreply.github.com>
2021-11-27 07:27:47 -0800

    Initial AWS Configuration

--- aws_infrastructure/aws_infrastructure/tasks/library/aws_configure.py
+++ aws_infrastructure/aws_infrastructure/tasks/library/aws_configure.py
@@ -2,12 +2,13 @@
 Tasks for configuring the AWS CLI environment.
 """
 
+import configparser
 from dataclasses import dataclass
+import dotenv
 from invoke import Collection
 from invoke import task
 from pathlib import Path
 from typing import Dict
-from typing import List
 from typing import Union
 
 
@@ -24,18 +25,44 @@ class AWSConfig:
     """
 
 
-def _task_configure(*, awsconfig_name: str, awsconfig: AWSConfig):
+def _task_configure(
+    *,
+    awsenv_path: Path,
+    awsconfig: AWSConfig,
+    awsconfig_name: str,
+):
     @task
     def configure(context):
         """
         Configure AWS CLI for {}.
         """
-        # if test.pipenv_pytest_dirs:
-        #     for test_dir in test.pipenv_pytest_dirs:
-        #         _pipenv_pytest(context=context, test_dir=test_dir)
-        pass
 
-    configure.__doc__ = configure.__doc__.format(test_name)
+        config = configparser.SafeConfigParser()
+        config.read(awsconfig.aws_config_path)
+
+        config_profile = config['profile {}'.format(awsconfig.profile)]
+
+        with open(awsenv_path, mode='w') as awsenv_file:
+            awsenv_file.writelines(['\n'.join([
+                '# Generated from:',
+                '#   config: {}'.format(awsconfig.aws_config_path),
+                '#   profile: {}'.format(awsconfig.profile),
+                '',
+                '{}={}'.format(
+                    'AWS_ACCESS_KEY_ID',
+                    config_profile['aws_access_key_id'],
+                ),
+                '{}={}'.format(
+                    'AWS_SECRET_ACCESS_KEY',
+                    config_profile['aws_secret_access_key'],
+                ),
+                '{}={}'.format(
+                    'AWS_DEFAULT_REGION',
+                    config_profile['region'],
+                ),
+            ])])
+
+    configure.__doc__ = configure.__doc__.format(awsconfig_name)
 
     return configure
 
@@ -43,14 +70,30 @@ def _task_configure(*, awsconfig_name: str, awsconfig: AWSConfig):
 def create_tasks(
     *,
     config_key: str,
+    awsenv_path: Union[Path, str],
     awsconfigs: Dict[str, AWSConfig],
 ):
     ns = Collection('configure')
 
     for (awsconfig_name_current, awsconfig_current) in awsconfigs.items():
         ns.add_task(
-            _task_configure(awsconfig_name=awsconfig_name_current, awsconfig=awsconfig_current),
+            _task_configure(
+                awsenv_path=awsenv_path,
+                awsconfig=awsconfig_current,
+                awsconfig_name=awsconfig_name_current,
+            ),
             name=awsconfig_name_current,
         )
 
     return ns
+
+
+def apply_awsenv(
+    *,
+    awsenv_path: Union[Path, str],
+):
+    """
+    Apply the environment at the provided path, created by a prior configure task.
+    """
+    if Path(awsenv_path).exists():
+        dotenv.load_dotenv(dotenv_path=awsenv_path, override=True, verbose=True)
